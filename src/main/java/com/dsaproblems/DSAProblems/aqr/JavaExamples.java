360f3834db9fc9d5a3d2351b9dd04c34559a4af2
Pratik Phookan <pratikphookan@Pratiks-Laptop.local>
2025-10-12 21:24:06 +0530

    add classes

---
+++ src/main/java/com/dsaproblems/DSAProblems/aqr/JavaExamples.java
@@ -0,0 +1,10 @@
+package com.dsaproblems.DSAProblems.aqr;
+
+public class JavaExamples {
+
+    public static void main(String[] args) {
+        byte by = 127;
+        by++;
+        System.out.println(by);
+    }
+}
